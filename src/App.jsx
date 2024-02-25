8826b6aec9493733480447bf7e8ec1b583ab5f8a
StanislavQA <ssv0123456@gmail.com>
2024-02-24 20:57:40 -0500

    lesson_2_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import "./App.css";
-
-var todoList = [
-  {
-    id: 1,
-    title: "Complete assignment #1",
-  },
-  {
-    id: 2,
-    title: "Complete assignment #2",
-  },
-  {
-    id: 3,
-    title: "Complete assignment #3",
-  },
-];
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (todo) {
-          return <li key={todo.id}>{todo.title}</li>;
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </>
   );
 }
