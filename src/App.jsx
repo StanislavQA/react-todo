a461c04d7ae8c38b4c0294b00b54206c668bba1a
StanislavQA <ssv0123456@gmail.com>
2024-05-09 14:57:04 -0400

    lesson_1_8

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,44 @@ function App() {
 
   React.useEffect(() => {
     // Side-effect with an empty dependency list
-    const fetchData = () => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          const storedData = localStorage.getItem("todoList");
-          const parsedData = storedData ? JSON.parse(storedData) : []; //Added by myself, not sure if it should look like this
-          resolve({ data: { todoList: parsedData } }); // Pass todoList as initial/default list state
-        }, 2000);
-      });
+    const fetchData = async () => {
+      try {
+        // Create URL using enviroment variables
+        const url = `https://api.airtable.com/v0/${
+          import.meta.env.VITE_AIRTABLE_BASE_ID
+        }/${import.meta.env.VITE_TABLE_NAME}`;
+        console.log(url);
+        // Declare an empty object variable named options
+        const options = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+          },
+        };
+
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Airtable API response:", data); //Print out the data variable
+        return { data };
+      } catch (error) {
+        // Handle error e.g., show an error message to the user
+        console.error("Error fetching data:", error);
+        throw error;
+      }
     };
 
-    fetchData().then((result) => {
-      setTodoList(result.data.todoList); // Update todoList with the todoList from the result object
-      setIsLoading(false); // Set isLoading to false after data fetch
-    });
+    fetchData()
+      .then((result) => {
+        setTodoList(result.data.todoList); // Update todoList with the todoList from the result object
+        setIsLoading(false); // Set isLoading to false after data fetch
+      })
+      .catch((error) => {
+        //Handle error, e.g., set isLoading to false to stop loading state
+        setIsLoading(false);
+      });
   }, []);
 
   // Function to add a new todo to the todoList state
